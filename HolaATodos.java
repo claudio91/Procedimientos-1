8f37272739839e67c3637005db675dc39549ebe7
CLAUDIO <claudio.vargas25@inacapmail.c>
2015-06-15 15:30:25 -0300

    Hola a Todos
    a) Crear un procedimiento llamado hola que dibuje por pantalla "Hola a
    Todos"

---
+++ HolaATodos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.a.todos;
+
+/**
+ *
+ * @author Claudio
+ */
+public class HolaATodos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+    Hola();  // AQUI SIMPLEMENTE SE INVOCA NUESTRO METODO CREADO LLAMDO HOLA
+    }
+    public static void Hola (){ //Esto es un METODO LLAMADO HOLA
+        System.out.println("Hola a Todos"); // SALIDA ESTANDAR CON UN MENSAJE ¡HOLA A TODOS !
+    }
+}
+// AL EJECUTAR EL PROGRAMA APARECERA UN MESAJE DICIENDO ¡HOLA A TODOS!
